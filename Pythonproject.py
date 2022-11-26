d2ba4ce80d3df46454b3bde3876a82e8fbf517a0
Harpreet singh <119067585+Harpreet12215827@users.noreply.github.com>
2022-11-26 10:45:20 +0530

    Update Pythonproject.py

--- Pythonproject.py
+++ Pythonproject.py
@@ -1,10 +1,24 @@
+#Clock Angle Problem
+#Clock Angle Problem: Given time in hh:mm format in 24-hour notation, calculate the
+#shorter angle between the hour and minute hand in an analog clock.
+
 def find_angle(hh, mm):
+    #handle 24-hour notation
     hh = hh % 12
+    
+    #find the position of hour hand
     h = (hh*360)//12+(mm*360)//(12*60)
+    
+    #finf the position of minutes's hand
     m = (mm*360)//60
+    
+    #calculate the angle difference 
     angle = abs(h-m)
+    
+    #consider the shorter angle and return it
     if angle > 180:
         angle = 360 - angle
+        
     return angle
 
 
